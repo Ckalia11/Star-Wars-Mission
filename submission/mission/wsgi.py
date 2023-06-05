b53cbdc2cab94bfabc82fab740a0b89927fb4179
Chetan <ckalia@uwaterloo.ca>
2023-06-05 03:21:27 -0400

    added req.txt and renamed project

--- submission/mission/wsgi.py
+++ submission/mission/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for giskard project.
+WSGI config for mission project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'giskard.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mission.settings')
 
 application = get_wsgi_application()
